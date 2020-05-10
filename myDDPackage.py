40b62b029323fd569881fb2d4f23b187738f5e3a
Denis Dobos <ddobos40@gmail.com>
2020-05-11 00:20:47 +0300

    add methode reverceString and lenghtString

---
+++ myDDPackage.py
@@ -0,0 +1,12 @@
+
+def reverceString(str):
+    return (str[::-1])
+
+def lengthString(str):
+    len = 0
+    for i in str:
+        len += 1
+    return len
+
+print(reverceString('La'))
+print(lengthString('La'))
